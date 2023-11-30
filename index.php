e9c008c43c2e9baec671053e29c4ee84c5fabfe0
guhan2104 <61934192+guhan2104@users.noreply.github.com>
2023-11-30 11:29:32 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Signup Form</title>
+</head>
+<body>
+    <h2>Signup Form</h2>
+    <form action="signup.php" method="post">
+        Email: <input type="email" name="email" required><br>
+        Username: <input type="text" name="username" required><br>
+        Password: <input type="password" name="password" required><br>
+        <input type="submit" value="Signup">
+    </form>
+</body>
+</html>
